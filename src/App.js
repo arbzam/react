8846ed56582ca18739198672cd8d04696d1dce66
arbzam <48916184+arbzam@users.noreply.github.com>
2021-03-25 13:55:08 +0500

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
+import {BrowserRouter as Router , Switch, Route, Redirect} from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Topbar from './components/Topbar/Topbar';
+import Navbar from './components/Navbar/Navbar';
+import Footer from './components/Footer/Footer';
+import Copyright from './components/Copyright/Copyright';
+import Packages from "./pages/Packages";
+import Contact from './pages/Contact';
 import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <>
+    <Router>
+    <Topbar />
+    <Navbar />
+    <Switch>
+      <Route path="/" exact component={Home}></Route>
+      <Route path="/about" exact component={About}></Route>
+      <Route path="/packages" exact component={Packages}></Route>
+      <Route path="/contact" exact component={Contact}></Route>
+      <Redirect exact to="/"></Redirect>
+    </Switch>
+    <Footer />
+    <Copyright />
+    </Router>
+   </>
   );
 }
-
 export default App;
